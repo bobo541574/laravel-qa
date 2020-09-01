0dd52e5c93fd95cbbffc4cc59e586ecc5b4fb574
bobo541574 <bobo541574.dev@gmail.com>
2020-09-01 14:15:03 +0630

    Question store & message

--- app/Question.php
+++ app/Question.php
@@ -13,10 +13,31 @@ class Question extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,15 +5,43 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Question</div>
+                <div class="card-header">
+                    <div class="d-flex align-item-center">
+                        <h2>All Question</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">
+                                Ask Question
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
+                    @include('layouts._messages')
                     @foreach ($questions as $question)
                     <div class="media">
+                        <div class="d-flex flex-column counters">
+                            <div class="vote">
+                                <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
+                            </div>
+                            <div class="status {{ $question->status }}">
+                                <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers) }}
+                            </div>
+                            <div class="view">
+                                {{ $question->views . " " . Str::plural('view', $question->views) }}
+                            </div>
+                        </div>
                         <div class="media-body">
                             <h3 class="mt-0">
-                                {{ $question->title }}
+                                <a href="{{ $question->url }}">
+                                    {{ $question->title }}
+                                </a>
                             </h3>
+                            <p class="load">
+                                Asked by
+                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>    
+                                <small class="text-muted">{{ $question->created_date }}</small>
+                            </p>
                             {{ substr($question->body, 0, 250) }}
                         </div>
                     </div>
