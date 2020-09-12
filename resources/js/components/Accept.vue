f3baddf0ee154a89a4aed0b43ec885583fb6acb3
bobo541574 <bobo541574.dev@gmail.com>
2020-09-12 19:55:21 +0630

    EventBus for Accept Answer by Authenicated User

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import EventBus from "../event-bus";
+
 export default {
   props: ["answer"],
   data() {
@@ -27,6 +29,13 @@ export default {
       id: this.answer.id,
     };
   },
+
+  created() {
+    EventBus.$on("accepted", (id) => {
+      this.isBest = id === this.id;
+    });
+  },
+
   computed: {
     canAccept() {
       return this.authorize("accept", this.answer);
@@ -46,6 +55,8 @@ export default {
           position: "bottomLeft",
         });
         this.isBest = true;
+
+        EventBus.$emit("accepted", this.id);
       });
     },
   },
